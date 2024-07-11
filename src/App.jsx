0aa4e29da34c5c89050c282b18be824c21904506
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-07-11 15:39:56 +0900

    searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import "./App.css";
 import PokeCard from "./components/PokeCard";
 
 function App() {
-  const [pokemons, setpokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(20);
+  const [searchTerm, setSearchTerm] = useState();
 
   useEffect(() => {
     fetchPokeData(true);
@@ -20,7 +21,7 @@ function App() {
 
       const response = await axios.get(url);
       // console.log(response.data.results);
-      setpokemons([...pokemons, ...response.data.results]);
+      setPokemons([...pokemons, ...response.data.results]);
       setOffset(offsetValue);
     } catch (error) {
       console.error(error);
@@ -28,11 +29,48 @@ function App() {
   };
   console.log(offset);
 
+  const handleSearchInput = async (e) => {
+    setSearchTerm(e.target.value);
+    if (e.target.value.length > 0) {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${e.target.value}`
+        );
+        const pokemonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+          name: searchTerm,
+        };
+        setPokemons([pokemonData]);
+        console.log(e.target.value);
+      } catch (error) {
+        setPokemons([]);
+        console.error(error);
+      }
+    } else {
+      fetchPokeData(true);
+    }
+  };
+
   return (
     <>
       <article className="pt-6">
         <header className="flex flex-col gap-2 w-full px-4 z-50">
-          Input form
+          <div className="relative z-50">
+            <form className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchInput}
+                className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center"
+              />
+              <button
+                type="submit"
+                className="text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700"
+              >
+                search
+              </button>
+            </form>
+          </div>
         </header>
         <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
           <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
