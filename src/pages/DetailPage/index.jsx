ae77281d54e7be4690c98333b8e72482d26e2765
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-07-12 17:45:59 +0900

    img_card

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Loading } from "../../assets/Loading";
+import { LessThan } from "../../assets/LessThan";
+import { GreaterThan } from "../../assets/GreaterThan";
+import { ArrowLeft } from "../../assets/ArrowLeft";
 
 const DetailPage = () => {
   const [pokemon, setPokemon] = useState();
@@ -43,6 +47,7 @@ const DetailPage = () => {
           abilities: formatPokemonAbilities(abilities),
           stats: formatPokemonStats(stats),
           DamageRelations,
+          types: types.map((type) => type.type.name),
         };
 
         setPokemon(formattedPokemonData);
@@ -50,6 +55,7 @@ const DetailPage = () => {
       }
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   }
 
@@ -95,9 +101,74 @@ const DetailPage = () => {
     };
   }
 
-  if (isLoading) return <div>...Loading</div>;
+  if (isLoading) {
+    return (
+      <div
+        className={`absolute h-auto w-auto top-1/3 -translate-x-1/2 left-1/2 z-50`}
+      >
+        <Loading className="w-12 h-12 z-50 animate-spin text-slate-900" />
+      </div>
+    );
+  }
 
-  return <div>DatailPage</div>;
+  if (!isLoading && !pokemon) {
+    return <div>...NOT FOUND</div>;
+  }
+
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+  const bg = `bg-${pokemon?.types?.[0]}`;
+  const text = `text-${pokemon?.types?.[0]}`;
+  return (
+    <article className="flex items-center gap-1 flex-col w-full">
+      <div
+        className={`${bg} w-auto h-full flex flex-col z-0 items-center justify-end relative overflow-hidden`}
+      >
+        {pokemon.previous && (
+          <Link
+            className="absolute top-[40%] -translate-y-1/2 z-50 left-1"
+            to={`/pokemon/${pokemon.previous}`}
+          >
+            <LessThan className="w-5 h-8 p-1" />
+          </Link>
+        )}
+
+        {pokemon.next && (
+          <Link
+            className="absolute top-[40%] -translate-y-1/2 z-50 right-1"
+            to={`/pokemon/${pokemon.next}`}
+          >
+            <GreaterThan className="w-5 h-8 p-1" />
+          </Link>
+        )}
+
+        <section className="w-full flex flex-col z-20 items-center justify-end relative h-full">
+          <div className="absolute z-30 top-6 flex items-center w-full justify-between px-2">
+            <div className="flex items-center gap-1">
+              <Link to="/">
+                <ArrowLeft className="w-6 h-8 text-zinc-200"></ArrowLeft>
+              </Link>
+              <h1 className="text-zinc-200 font-bold text-xl capitalize">
+                {pokemon.name}
+              </h1>
+            </div>
+            <div className="text-zinc-200 font-bold text-md">
+              #{pokemon.id.toString().padStart(3, "00")}
+            </div>
+          </div>
+          <div className="relative h-auto max-w-[15.5rem] z-20 mt-6 -mb-16">
+            <img
+              src={img}
+              width="100%"
+              height="auto"
+              loading="lazy"
+              alt={pokemon.name}
+              className={`object-contain h-full`}
+            />
+          </div>
+        </section>
+      </div>
+    </article>
+  );
 };
 
 export default DetailPage;
