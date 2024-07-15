ef67b0a54c95790d0ca4e3d07cb4c71949e6c57c
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-07-15 16:18:41 +0900

    DR_screen

--- src/components/DamageRelations.jsx
+++ src/components/DamageRelations.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Type from "../components/Type";
 
 const DamageRelations = ({ damages }) => {
   const [damagePokemonForm, setDamagePokemonForm] = useState();
@@ -116,7 +117,42 @@ const DamageRelations = ({ damages }) => {
     return result;
   };
 
-  return <div>DamageRelations</div>;
+  return (
+    <div className="flex gap-2 flex-col">
+      {damagePokemonForm ? (
+        <>
+          {Object.entries(damagePokemonForm).map(([keyName, value]) => {
+            const key = keyName;
+            const valuesOfKeyName = {
+              double_damage: "weak",
+              half_damage: "Resistant",
+              no_damage: "Immune",
+            };
+            return (
+              <div key={key}>
+                <h3 className="capitalize font-medium text-sm md:text-base text-slate-500 text-center">
+                  {valuesOfKeyName[key]}
+                </h3>
+                <div className="flex flex-wrap gap-1 justify-center">
+                  {value.length > 0 ? (
+                    value.map(({ name, url, damageValue }) => {
+                      return (
+                        <Type type={name} key={url} damageValue={damageValue} />
+                      );
+                    })
+                  ) : (
+                    <Type type={"none"} key={"none"} />
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default DamageRelations;
