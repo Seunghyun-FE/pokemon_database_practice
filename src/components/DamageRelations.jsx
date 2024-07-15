f872f4291242f44ae2812b79f2577db96acc7fdc
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-07-15 14:24:57 +0900

    DR_two_value_plus

--- src/components/DamageRelations.jsx
+++ src/components/DamageRelations.jsx
@@ -1,14 +1,100 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useState } from "react";
 
 const DamageRelations = ({ damages }) => {
-  console.log("@@@@", damages);
+  const [damagePokemonForm, setDamagePokemonForm] = useState();
 
   useEffect(() => {
     const arrayDamage = damages.map((damage) =>
       separateObjectBetweenToandFrom(damage)
     );
+
+    if (arrayDamage.length === 2) {
+      //2가지 속성일 때 데미지 from/to를 합침
+      const obj = joinDamageRelations(arrayDamage);
+      // console.log(obj);
+
+      setDamagePokemonForm(reduceDuplicateValues(postDamageValue(obj.from))); //2가지 속성일 때 중복된 속성 항목은 합치고 배수 맞춰주기
+    } else {
+      setDamagePokemonForm(postDamageValue(arrayDamage[0].from));
+    }
   }, []);
 
+  const joinDamageRelations = (props) => {
+    return {
+      to: joinObject(props, "to"),
+      from: joinObject(props, "from"),
+    };
+  };
+
+  const reduceDuplicateValues = (props) => {
+    const duplicateValues = {
+      double_damage: "4x",
+      half_damage: "1/4x",
+      no_damage: "0x",
+    };
+    return Object.entries(props).reduce((acc, [keyName, value]) => {
+      const key = keyName;
+      // console.log([keyName, value]);
+      const verifiedValue = filterForUniqueValues(value, duplicateValues[key]);
+      return (acc = { [keyName]: verifiedValue, ...acc });
+    }, {});
+  };
+
+  const filterForUniqueValues = (valueForFiltering, damageValue) => {
+    // console.log(valueForFiltering, damageValue);
+
+    return valueForFiltering.reduce((acc, currentValue) => {
+      // 속성만 쌓기
+      const { url, name } = currentValue;
+      // console.log(url, name);
+      const filterACC = acc.filter((a) => a.name !== name); // name을 필터링
+      return filterACC.length === acc.length
+        ? (acc = [currentValue, ...acc])
+        : (acc = [{ damageValue: damageValue, name, url }, ...filterACC]);
+    }, []);
+  };
+
+  const joinObject = (props, string) => {
+    const key = string;
+    const firstArrayValue = props[0][key];
+    const secondArrayValue = props[1][key];
+    // console.log("props", props);
+    // console.log("firstArrayValue", firstArrayValue);
+    const result = Object.entries(secondArrayValue).reduce(
+      (acc, [keyName, value]) => {
+        // console.log(acc, [keyName, value]);
+        const result = firstArrayValue[keyName].concat(value); //keyname(더블/하프데미지 등)에 맞게 1,2배열이 합쳐짐
+        return (acc = { [keyName]: result, ...acc }); // keyname별로 순회하면서 배열이 쌓임
+      },
+      {}
+    );
+    // console.log(result);
+    return result;
+  };
+
+  //1타입 데미지 관계
+  const postDamageValue = (props) => {
+    const result = Object.entries(props).reduce((acc, [keyName, value]) => {
+      const key = keyName;
+      const valuesOfKeyName = {
+        double_damage: "2x",
+        half_damage: "1/2x",
+        no_damage: "0x",
+      };
+
+      return (acc = {
+        [keyName]: value.map((i) => ({
+          damageValue: valuesOfKeyName[key],
+          ...i,
+        })),
+        ...acc,
+      });
+    }, {});
+    return result;
+    // console.log(result);
+  };
+
+  //from, to로 데이터 가공
   const separateObjectBetweenToandFrom = (damage) => {
     const from = filterDamageRelations("_from", damage);
 
@@ -17,17 +103,14 @@ const DamageRelations = ({ damages }) => {
     return { from, to };
   };
 
+  //데이터 가공 함수
   const filterDamageRelations = (valueFilter, damage) => {
     const result = Object.entries(damage)
       .filter(([keyname, value]) => {
-        console.log("keyname", keyname);
-        console.log("value", value);
-
         return keyname.includes(valueFilter);
       })
       .reduce((acc, [keyName, value]) => {
         const keyWithValueFilterRemove = keyName.replace(valueFilter, "");
-        console.log(acc);
         return (acc = { [keyWithValueFilterRemove]: value, ...acc });
       }, {});
     return result;
