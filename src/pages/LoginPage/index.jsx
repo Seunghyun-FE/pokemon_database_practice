dbe4fa5dec93e75c6eb883960eaa86d9e9c31609
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-07-20 21:36:09 +0900

    Login_page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <section className="bg-gray-50 min-h-[90vh] flex items-center justify-center">
+      <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+        <div className="md:w-1/2 px-8 md:px-16">
+          <h2 className="font-bold text-2xl ">Pokemon</h2>
+          <p className="text-xs mt-4 text-[#002D74]">Welcome</p>
+          <p className="text-xs mt-4 text-[#002D74]">well</p>
+          <p className="text-xs mt-4 text-[#002D74]">Plz Login</p>
+        </div>
+        <div className="md:block hidden w-1/2">
+          <img
+            alt="login"
+            className="rounded-2xl "
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/89.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LoginPage;
